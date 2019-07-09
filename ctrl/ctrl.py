74c82fc82489fe38c37967adca4c4c08311af14d
Sauhaarda Chowdhuri <sauhaarda@gmail.com>
2019-07-09 15:21:53 -0700

    Started adding skeleton ctrl.py

---
+++ ctrl/ctrl.py
@@ -0,0 +1,26 @@
+
+def generate_control_action(x, u_m):
+    """Generate control action given state and motion primitive
+
+    Parameters:
+    x (np.ndarray): the current state of the system
+    u_m (np.ndarray): the motion primitive
+
+    Returns:
+    u_c (np.ndarray): the control action for input into dynamics
+    """
+
+    pass
+
+def simulate_dynamics(x, u_c):
+    """Get the next state given the control action and current state
+
+    Parameters:
+    x (np.ndarray): the current state of the system
+    u_c (np.ndarray): the control action
+
+    Returns:
+    x_next (np.ndarray): the control action for input into dynamics
+    """
+
+    pass
